c2388753d8a347c5cdf031d89b982724fc119d85
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-01 01:04:29 +0800

    complete hero image

--- client/src/components/landing/Hero.tsx
+++ client/src/components/landing/Hero.tsx
@@ -1,13 +1,15 @@
 import Container from '@/globals/Container'
 import { Link } from 'react-router-dom'
 import { Button } from '../ui/button'
+import heroImg from '@/assets/images/hero.png'
 
 const Hero = () => {
   return (
-    <Container className='py-20'>
-      <div className="flex items-center justify-between gap-x-6 ">
-        <section>
-          <h1 className='text-4xl lg:text-5xl font-bold'>Selamat Datang Di Perpustakaan <br /> Teknik Informatika</h1>
+    <Container className='my-10 bg-none lg:bg-[url("/images/bg.png")] bg-cover bg-cente'>
+      <div className="flex items-center justify-between gap-x-6">
+        
+        <section className='w-fit'>
+          <h1 className='text-4xl lg:text-5xl font-semibold leading-12 lg:leading-16'>Selamat Datang Di Perpustakaan <br /> Teknik Informatika</h1>
           <p className='w-full lg:w-[70%] mt-6 text-muted-foreground'>Nikmati akses ke beragam koleksi buku, jurnal, dan sumber digital yang mendukung studi dan penelitian Anda. Dengan fasilitas yang nyaman, perpustakaan ini siap menjadi ruang belajar yang inspiratif.</p>
 
           <div className='flex gap-x-4 mt-8'>
@@ -19,8 +21,9 @@ const Hero = () => {
           </div>
         </section>
 
-        <section className='hidden lg:block'>
-          <div className='w-[400px] h-[400px] bg-muted rounded-2xl'></div>
+        <section className='w-fit h-full hidden lg:flex justify-end'>
+          {/* <div></div> */}
+          <img src={heroImg} className='aspect-square rounded-2xl'/>
         </section>
       </div>
     </Container>
