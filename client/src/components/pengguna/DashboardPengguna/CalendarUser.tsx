0ee8b49104cf08426bbb9e90a46255a8aebaac4c
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-07 10:48:01 +0800

    refactor

--- client/src/components/pengguna/DashboardPengguna/CalendarUser.tsx
+++ client/src/components/pengguna/DashboardPengguna/CalendarUser.tsx
@@ -6,12 +6,12 @@ const CalendarPengguna = () => {
     const [date, setDate] = React.useState<Date | undefined>(new Date())
     
     return (
-        <div className='hidden lg:grid lg:col-span-3 w-fit place-self-center bg-primary-foreground dark:bg-card rounded-xl border'>
+        <div className='hidden lg:grid lg:col-span-3 w-full place-self-center bg-primary-foreground dark:bg-card rounded-xl border'>
             <Calendar
                 mode="single"
                 selected={date}
                 onSelect={setDate}
-                className="rounded-md border"
+                className="w-full flex items-center justify-center rounded-md border"
             />
         </div>
     )
--- client/src/components/pengguna/DashboardPengguna/RecommendationBook.tsx
+++ client/src/components/pengguna/DashboardPengguna/RecommendationBook.tsx
@@ -4,12 +4,12 @@ import { Eye, BookCheck, ThumbsUp } from 'lucide-react'
 const RecommendationBook = () => {
   return (
     <main className='hidden self-center w-[90%] lg:flex gap-x-6'>
-        <img className="h-40 w-28 object-fill rounded" src="https://res.cloudinary.com/dhthnjizr/image/upload/v1746458439/eeerieu4ivgzsylylaaj.jpg" alt="" />
+        <img className="h-40 w-28 object-fill rounded" src="https://res.cloudinary.com/dhthnjizr/image/upload/v1746585631/rljqdfpo7oljsit5zdyq.jpg" alt="" />
 
         <div className='flex flex-col'>
-            <h2 className='font-semibold text-xl lg:text-2xl'>Clean Code  </h2>
-            <h5 className='text-sm text-muted-foreground'>{'A Handbook of Agile Software Craftmanship'.slice(0,40)}...</h5>
-            <p className='text-sm text-muted-foreground mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod itaque mollitia quam dolorem, ullam architecto!</p>
+            <h2 className='font-semibold text-xl lg:text-2xl'>Eloquent JavaScript</h2>
+            <h5 className='text-sm text-muted-foreground'>{'A Modern Introduction to Programming'.slice(0,40)}...</h5>
+            <p className='text-sm text-muted-foreground mt-2'>{'Buku ini adalah pengantar pemrograman modern menggunakan JavaScript sebagai media utama. Ditujukan untuk pemula, namun juga sangat cocok bagi yang ingin memperdalam pemahaman akan konsep-konsep inti dalam pemrograman seperti fungsi, struktur data, rekursi, OOP, hingga asinkronitas.'.slice(0, 110)}....</p>
 
             <Separator className='mt-4' />
             <div className='w-full flex items-center justify-around text-sm mt-2'>
