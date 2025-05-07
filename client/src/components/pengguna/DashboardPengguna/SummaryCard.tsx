99738b49b8ea0c1caae57040d07adb06552f1542
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-07 09:55:42 +0800

    loan summary responsiveness

--- client/src/components/pengguna/DashboardPengguna/SummaryCard.tsx
+++ client/src/components/pengguna/DashboardPengguna/SummaryCard.tsx
@@ -4,14 +4,14 @@ import { Separator } from '@/components/ui/separator'
 
 const SummaryCard = () => {
   return (
-    <section className='w-full flex gap-x-4 mt-4'>
+    <section className='w-full grid grid-cols-12 mt-4 gap-x-4 gap-y-4'>
         {Array.from({length: 4}).map((_, index) => {
           return (
-            <Card key={index} className='w-1/4 p-4 bg-primary-foreground dark:bg-card'>
+            <Card key={index} className='col-span-6 lg:col-span-3 p-4 bg-primary-foreground dark:bg-card'>
               <section className='flex flex-row items-start justify-start gap-x-4'>
                 <Book className='h-10 w-10 border p-2 rounded bg-primary dark:bg-transparent stroke-white' />
                 <div>
-                  <h1 className='text-2xl font-bold tracking-widest'>100</h1>
+                  <h1 className='text-xl lg:text-2xl font-bold tracking-widest'>100</h1>
                   <p className='text-muted-foreground text-sm'>Buku Dipinjam</p>
                 </div>
               </section>
