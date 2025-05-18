e5aba58c8b42854e65bc8faa70a2e318436151fa
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-18 18:53:25 +0800

    adding conditional style in menu

--- client/src/components/pengguna/peminjamanPengguna/MenuContainer.tsx
+++ client/src/components/pengguna/peminjamanPengguna/MenuContainer.tsx
@@ -1,14 +1,18 @@
 import { dataMenuLinks } from '@/utils/constants'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const MenuContainer = () => {
+
+  const pathname = useLocation().pathname.split('/').pop()
+
   return (
     <section className='mt-20 flex flex-col gap-y-4'>
         {dataMenuLinks.map(item => {
             return (
                 <Link to={item.title} key={item.id} className='flex gap-x-4 items-center justify-start'>
                     {item.icon}
-                    <p className='capitalize'>{item.title}</p>
+                    <p className={`capitalize ${pathname === item.title ? 'text-primary' : ''}`}>{item.title}</p>
                 </Link>
             )
         })}
