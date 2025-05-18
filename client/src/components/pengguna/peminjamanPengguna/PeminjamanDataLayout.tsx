8aa3523baf502d0754ba5d0e4fece12ea4e68d3d
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-18 23:00:35 +0800

    fixing small bug while initiate data

--- client/src/components/pengguna/peminjamanPengguna/PeminjamanDataLayout.tsx
+++ client/src/components/pengguna/peminjamanPengguna/PeminjamanDataLayout.tsx
@@ -6,10 +6,12 @@ import { useRouteLoaderData } from 'react-router-dom'
 const PeminjamanDataLayout = () => {
   const {data} = useRouteLoaderData('peminjaman-data')
   const {layout, peminjamanFilter} = useSelector((state:any) => state.peminjamanState)
-  
+
+  const newFilter = peminjamanFilter ? peminjamanFilter : 'Semua'
+
   const newData = data.filter((item:any) => {
-    if (peminjamanFilter === 'Semua') return item
-    return item.statusPeminjaman === peminjamanFilter || 'Semua'
+    if (newFilter === 'Semua') return item
+    return item.statusPeminjaman === peminjamanFilter
   })
   return (
     <>
