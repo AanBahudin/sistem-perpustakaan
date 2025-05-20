14e1b4ac70972882545b25713b6321c6144a1da8
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-20 11:19:15 +0800

    setting up peminjaman search function

--- client/src/components/pengguna/peminjamanPengguna/PeminjamanSearch.tsx
+++ client/src/components/pengguna/peminjamanPengguna/PeminjamanSearch.tsx
@@ -1,11 +1,29 @@
 import { Search } from "lucide-react"
 import { Input } from "@/components/ui/input"
+import { useDebouncedCallback } from 'use-debounce'
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 const PeminjamanSearch = () => {
+
+  const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
+
+  const params = new URLSearchParams(searchParams)
+  const search = searchParams.get('search')
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    if (value) {
+      params.set('search', value)
+    } else {
+      params.delete('search')
+    }
+    navigate(`?${params.toString()}`);
+  }, 500)
+
   return (
     <div className="w-full flex items-center border my-6 rounded bg-muted">
         <Search className="bg-transparent mx-4 stroke-muted-foreground bg-mute" />
-        <Input className="w-full border-none focus:outline-none focus:ring-0 focus-visible:ring-0 !bg-muted rounded-none placeholder:text-muted-foreground focus:placeholder:text-transparent" placeholder="Kamu bisa mencari nama buku yang dipinjam" />
+        <Input defaultValue={search || ''} onChange={(e) => handleSearch(e.target.value)} className="w-full border-none focus:outline-none focus:ring-0 focus-visible:ring-0 !bg-muted rounded-none placeholder:text-muted-foreground focus:placeholder:text-transparent" placeholder="Kamu bisa mencari nama buku yang dipinjam" />
     </div>
   )
 }
