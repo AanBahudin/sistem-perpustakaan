1b339608ae40cbd2e1bfeafc129b09292e384882
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-23 14:47:55 +0800

    adding interactive in share button

--- client/src/components/pengguna/peminjamanPengguna/PeminjamanList.tsx
+++ client/src/components/pengguna/peminjamanPengguna/PeminjamanList.tsx
@@ -4,6 +4,8 @@ import { formatedDate } from "@/utils/formatDate"
 import { Link } from "react-router-dom"
 import StatusPeminjamanList from "./StatusPeminjamanList"
 import GlobalTooltip from "@/globals/GlobalTooltip"
+import PeminjamanKategori from "./PeminjamanKategori"
+import ShareBookDialog from "@/globals/ShareBookDialog"
 
 
 const PeminjamanList = ({data = []} : {data: Object[]}) => {
@@ -17,28 +19,27 @@ const PeminjamanList = ({data = []} : {data: Object[]}) => {
           const {buku, statusPeminjaman, durasiPeminjaman, berakhirPada, createdAt} = item
           const newDeskripsi = buku.deskripsi.slice(0,240) + '...'
 
-          let newKategori : Array<string> = buku.kategori
-          if (newKategori.length > 3) {
-            newKategori = newKategori.slice(0, 3)
-          }
+          
           return (
-            <Link to={`/my/peminjaman/${item._id}`} key={item._id} className="w-full h-full col-span-12 border rounded-2xl flex items-center gap-x-4 p-4 hover:shadow-2xl duration-200 ease-in-out group">
+            <section key={item._id} className="w-full h-full col-span-12 border rounded-2xl flex items-center gap-x-4 p-4 hover:shadow-2xl duration-200 ease-in-out group">
               <img src={buku.cover} className="w-24 h-32 object-fill rounded" />
 
               <div className="w-full flex flex-col items-start justify-stretch ">
               <div className="w-full flex items-center justify-between">
-                <h2 className="text-2xl font-semibold group-hover:underline">{buku.judul}</h2>
+                <Link to={`/my/peminjaman/${item._id}`} className="text-2xl font-semibold group-hover:underline">{buku.judul}</Link>
                 <div className="flex items-center gap-x-4">
                   <StatusPeminjamanList status={statusPeminjaman} />
                   <div className="flex items-center gap-x-2">
                       <GlobalTooltip text="Disukai">
-                          <ThumbsUp className="w-8 h-8 border p-2 rounded-lg hover:bg-muted duration-200 ease-in-out" />
+                        <ThumbsUp className="w-8 h-8 border p-2 rounded-lg hover:bg-muted duration-200 ease-in-out" />
                       </GlobalTooltip>
                       <GlobalTooltip text="Simpan">
-                          <BookMarked className="w-8 h-8 border p-2 rounded-lg hover:bg-muted duration-200 ease-in-out" />
+                        <BookMarked className="w-8 h-8 border p-2 rounded-lg hover:bg-muted duration-200 ease-in-out" />
                       </GlobalTooltip>
                       <GlobalTooltip text="Bagikan">
+                        <ShareBookDialog>
                           <Share2 className="w-8 h-8 border p-2 rounded-lg hover:bg-muted duration-200 ease-in-out" />
+                        </ShareBookDialog>
                       </GlobalTooltip>
                   </div>
                 </div>
@@ -48,16 +49,9 @@ const PeminjamanList = ({data = []} : {data: Object[]}) => {
               <div className="w-full flex items-center justify-between">
                 <main className="w-2/3">
                   <h5 className="text-sm text-muted-foreground">{newDeskripsi}</h5>
-
-                  <div className="w-full flex gap-x-2 items-center">
-                    {newKategori.map((item: string) => {
-                      return (
-                        <h5 className="mt-2 bg-muted w-fit px-6 border rounded py-1 text-[12px]">{item}</h5>
-                      )
-                    })}
-                  </div>
-
+                  <PeminjamanKategori kategori={buku.kategori} />
                 </main>
+
                 <Separator orientation="vertical" className="mx-2" />
                 <main className="self-center flex items-start flex-col justify-center w-1/3 gap-y-2">
                   <p className="flex gap-x-4 text-sm text-muted-foreground items-center">
@@ -77,7 +71,7 @@ const PeminjamanList = ({data = []} : {data: Object[]}) => {
                 </main>
               </div>
               </div>
-            </Link>
+            </section>
           )
       })}
     </div>
