de4f6a7b6ec6bff75133f93111d41e0428bbc384
Aan Bahudin <aan.bahudin11@gmail.com>
2025-05-31 19:51:09 +0800

    detail peminjaman starter

--- client/src/pages/pengguna/Peminjaman/PeminjamanDetailPage.tsx
+++ client/src/pages/pengguna/Peminjaman/PeminjamanDetailPage.tsx
@@ -1,9 +1,18 @@
 import Container from '@/globals/Container'
+import { useQueries } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
 
 const PeminjamanDetailPage = () => {
+
+  const {id} = useParams()
+
+  const results = useQueries({
+    queries: []
+  }) 
+
   return (
-    <Container>
-      <h1>Detail peminjaman</h1>
+    <Container className='my-20'>
+      <h1>Peminjaman</h1>
     </Container>
   )
 }
