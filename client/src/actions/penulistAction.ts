eb8480a53cf695db78e961b82cb099176eda8607
Aan Bahudin <aan.bahudin11@gmail.com>
2025-07-25 23:31:38 +0800

    small fix

--- client/src/actions/penulistAction.ts
+++ client/src/actions/penulistAction.ts
@@ -5,7 +5,5 @@ export const getAllPenulis = async() => {
     if (status >= 400) {
         return {message: 'Terjadi kesalahan', deskripsi: 'Silahkan periksa koneksi Internet Anda'}
     }
-
-    console.log(response.data)
     return response.data
 }
