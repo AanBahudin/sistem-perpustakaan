566a9b3171b26cce3918251f6d532563a074d9a5
Aan Bahudin <aan.bahudin11@gmail.com>
2025-08-06 11:16:28 +0800

    mahasiswa page setup

--- client/src/pages/pustakawan/pengguna/DaftarPenggunaMahasiswa.tsx
+++ client/src/pages/pustakawan/pengguna/DaftarPenggunaMahasiswa.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { getAllPenggunaDosen } from '@/actions/Pustakawan/PustakawanGetPenggunaActions'
+import GrafikPertumbuhanSemuaPengguna from '@/components/Pustakawan/Pengguna/SemuaPengguna/GrafikPertumbuhanSemuaPengguna'
+import SemuaPenggunaFilter from '@/components/Pustakawan/Pengguna/SemuaPengguna/SemuaPenggunaFilter'
+import PustakawanBreadCrumbs from '@/components/Pustakawan/PustakawanBreadCrumbs'
+import Container from '@/globals/Container'
+import { useQuery } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+import TableSemuaPengguna from './TableSemuaPengguna'
 
 const DaftarPenggunaMahasiswa = () => {
+  const [searchParams] = useSearchParams()
+  const params = new URLSearchParams(searchParams).toString()
+
+  const {data, isLoading} = useQuery({
+    queryKey: ['dosen', 'pengguna', params],
+    queryFn: () => getAllPenggunaDosen(params)
+  })
+
+  if (isLoading) return <h1>Loading...</h1>
+
   return (
-    <div>DaftarPenggunaMahasiswa</div>
+    <Container className='w-full'>
+      <PustakawanBreadCrumbs />
+      <GrafikPertumbuhanSemuaPengguna graphFor='dosen' title='Statistik Pertumbuhan Dosen Bulanan' monthlyUserGrowData={data.monthlyUserGrowth} userAccountStatusRatio={data.userAccountStatusRatio}/>
+      <SemuaPenggunaFilter usedIn='dosen' />
+      <TableSemuaPengguna dataPengguna={data.pengguna} />
+    </Container>
   )
 }
 
