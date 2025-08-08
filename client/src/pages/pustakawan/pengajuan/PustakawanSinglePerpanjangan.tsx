292f0a3a2606ad39dda939176ae4533e74b97af0
Aan Bahudin <aan.bahudin11@gmail.com>
2025-08-08 23:59:52 +0800

    setup single data

---
+++ client/src/pages/pustakawan/pengajuan/PustakawanSinglePerpanjangan.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PustakawanSinglePerpanjangan = () => {
+  return (
+    <div>PustakawanSinglePerpanjangan</div>
+  )
+}
+
+export default PustakawanSinglePerpanjangan
