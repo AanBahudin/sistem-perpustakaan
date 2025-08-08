5220d504198c03628dcae422e3fa6a1a6972e1c2
Aan Bahudin <aan.bahudin11@gmail.com>
2025-08-08 15:15:03 +0800

    add status verifikasi akun

--- client/src/components/Pustakawan/Pengguna/SinglePengguna/InformasiKontakSinglePengguna.tsx
+++ client/src/components/Pustakawan/Pengguna/SinglePengguna/InformasiKontakSinglePengguna.tsx
@@ -1,12 +1,17 @@
+import { Badge } from "@/components/ui/badge"
 import { Separator } from "@/components/ui/separator"
-import { IdCard } from "lucide-react"
+import { Check, IdCard, LucideIcon, X } from "lucide-react"
 
 const InformasiKontakSinglePengguna = ({dataPengguna} : {dataPengguna: any}) => {
 
-    const { no_hp, email } = dataPengguna
+    const { no_hp, email, verifikasiEmail, verifikasiProdi } = dataPengguna
+    const IconVerifikasiEmail : LucideIcon = verifikasiEmail ? Check : X
+    const IconVerifikasiProdi : LucideIcon = verifikasiProdi ? Check : X
+    const emailBadgeBg = verifikasiEmail ? 'default' : 'destructive' 
+    const prodiBadgeBg = verifikasiProdi ? 'default' : 'destructive' 
 
     return (
-        <main className="w-full bg-primary/20 min-h-[25vh] rounded-xl p-4">
+        <section className="w-full bg-primary/20 min-h-[25vh] rounded-xl p-4">
             <h1 className="text-sm font-light flex items-center justify-start gap-x-2">
                 <IdCard />
                 Informasi Kontak
@@ -14,7 +19,7 @@ const InformasiKontakSinglePengguna = ({dataPengguna} : {dataPengguna: any}) => 
 
             <h5 className="text-xs my-3">Kontak Pribadi</h5>
                         
-            <div className="w-full flex items-start justify-between">
+            <main className="w-full flex items-start justify-between">
                 <div className="w-full flex flex-col items-start">
                     <h5 className="text-xs text-muted-foreground">Nomor Telepon</h5>
                     <p className="min-w-fit max-w-[20%] mt-2 bg-primary/20 text-center p-1 rounded text-[10px]">{no_hp || 'Tidak disertakan'}</p>
@@ -24,13 +29,22 @@ const InformasiKontakSinglePengguna = ({dataPengguna} : {dataPengguna: any}) => 
                     <h5 className="text-xs text-muted-foreground">Email</h5>
                     <p className="min-w-fit max-w-[20%] mt-2 bg-primary/20 text-center p-1 rounded text-[10px]">{email || 'Tidak disertakan'}</p>
                 </div>
-            </div>
+            </main>
 
             <Separator className="my-3" />
 
-            <h5 className="text-xs my-3">Kontak Pribadi</h5>
-            <h5 className="text-xs italic text-muted-foreground">Tidak ada</h5>
-        </main>
+            <h5 className="text-xs my-3">Status Verifikasi</h5>
+            <main className="w-full flex items-center justify-between gap-x-6">
+                <Badge variant={emailBadgeBg} className="flex-1 text-white flex items-center gap-x-2">
+                    <IconVerifikasiEmail className="w-4 h-4" />
+                    Email
+                </Badge>
+                <Badge variant={prodiBadgeBg} className="flex-1 text-white flex items-center gap-x-2">
+                    <IconVerifikasiProdi className="w-4 h-4" />
+                    Program Studi
+                </Badge>
+            </main>
+        </section>
     )
 }
 
