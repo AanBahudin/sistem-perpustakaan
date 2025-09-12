d61515de387fcbe27eb29625b9a1962c5e5b122b
Aan Bahudin <aan.bahudin11@gmail.com>
2025-09-13 06:28:49 +0800

    setup for add pustakawan dialog

--- client/src/pages/prodi/pustakawan/SemuaPustakawan.tsx
+++ client/src/pages/prodi/pustakawan/SemuaPustakawan.tsx
@@ -4,11 +4,19 @@ import Container from '@/globals/Container'
 
 import SearchAndFilterPustakawanDataContainer from '@/components/Prodi/Pustakawan/SearchAndFilterPustakawanDataContainer'
 import PustakawanDataContainer from '@/components/Prodi/Pustakawan/PustakawanDataContainer'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
 
 const SemuaPustakawan = () => {
   return (
     <Container className='w-full'>
-      <PustakawanBreadCrumbs />
+      <section className='flex items-center justify-between'>
+        <PustakawanBreadCrumbs />
+        <Button className='flex items-center gap-x-2 text-xs mb-4'>
+          <Plus />
+          <p>Tambah Pustakawan</p>
+        </Button>  
+      </section>
       <h4 className='mt-10 mb-4 text-2xl font-semibold'>Daftar Pustakawan Aktif</h4>
       <SearchAndFilterPustakawanDataContainer />
       <PustakawanDataContainer />
