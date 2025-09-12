476282a6ce5c9288d52a9fab7935a01642fe7fb3
Aan Bahudin <aan.bahudin11@gmail.com>
2025-09-13 06:26:01 +0800

    fixing query not refetching after perform a request

--- client/src/components/Prodi/Pustakawan/AktifkanPustakawanAlert.tsx
+++ client/src/components/Prodi/Pustakawan/AktifkanPustakawanAlert.tsx
@@ -21,7 +21,7 @@ const AktifkanPustakawanAlert = ({idPustakawan} : {idPustakawan: string}) => {
 
     const queryClient = useQueryClient()
     const [searchParams] = useSearchParams()
-    const params = new URLSearchParams(searchParams)
+    const params = new URLSearchParams(searchParams).toString()
     const { aktifkanAlert } = useSelector((state: any) => state.prodiPustakawanSlice)
     const handleOpenAlert = (value: boolean) => {
         store.dispatch(setAktifAlert({pustakawanId: idPustakawan, alertState: value}))
@@ -48,16 +48,16 @@ const AktifkanPustakawanAlert = ({idPustakawan} : {idPustakawan: string}) => {
         <AlertDialog open={aktifkanAlert} onOpenChange={handleOpenAlert}>
             <AlertDialogContent>
                 <AlertDialogHeader>
-                    <AlertDialogTitle>Nonaktifkan Akun Pustakawan?</AlertDialogTitle>   
+                    <AlertDialogTitle>Aktifkan Akun Pustakawan?</AlertDialogTitle>   
                     <AlertDialogDescription>
-                        Apakah Anda yakin ingin menonaktifkan akun pustakawan ini? Setelah dinonaktifkan, pustakawan tidak dapat lagi mengakses sistem perpustakaan hingga akun diaktifkan kembali oleh admin.
+                        Apakah Anda yakin ingin mengaktifkan kembali akun pustakawan ini? Akun yang diaktifkan ulang akan dapat digunakan kembali untuk mengakses sistem perpustakaan.
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
                     <AlertDialogCancel disabled={mutation.isPending}>Batal</AlertDialogCancel>
-                    <Button disabled={mutation.isPending} onClick={handleMutate} variant='destructive' className="flex items-center justify-center gap-x-2">
+                    <Button disabled={mutation.isPending} onClick={handleMutate} variant='default' className="flex items-center justify-center gap-x-2">
                         {mutation.isPending && <Loader className="animate-spin" />}
-                        {mutation.isPending ? 'Menonaktifkan' : 'Nonaktifkan'}
+                        {mutation.isPending ? 'Menaktifkan...' : 'Aktifkan'}
                     </Button>
                 </AlertDialogFooter>
             </AlertDialogContent>
