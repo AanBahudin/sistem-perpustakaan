9295885c75bde0f03a79336855a0e84eb0f68bdf
Aan Bahudin <aan.bahudin11@gmail.com>
2025-11-23 00:52:35 +0800

    refactor simpan button page

--- client/src/globals/AddSimpananButton.tsx
+++ client/src/globals/AddSimpananButton.tsx
@@ -1,41 +1,22 @@
 import { BookMarked, Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { addOrRemoveSimpanan, getAllSimpanan } from "@/actions/simpanActions"
-import { useState } from "react"
+import useSimpanBukuPengguna from "@/hooks/fetchHooks/penggunaHooks/simpanHooks/useSimpanBukuPengguna"
 
 const AddSimpananButton = ({id} : {id: string}) => {
 
-    const queryClient = useQueryClient()
-    const [loading, setLoading] = useState(false)
+    // REFACTOR KOMPONEN INI
+    /* 
+        komponen ini dipake berulang pada berbagai halaman dengan jumlah yang banyak dalam sekali render
+        yang mengakibatkan terjadinya fetch data yang sangat banyak, fungsi mutation nya meng-trigger
+        banyak sekali re-render. 
+    */
 
-    const {data, isLoading: dataLoading} = useQuery({
-        queryKey: ['simpan'],
-        queryFn: getAllSimpanan
-    })
+    const {
+        mutationFn, mutationLoading,
+        queryLoading, isIncludes
+    } = useSimpanBukuPengguna({idBuku: id})
 
-    const {mutateAsync: addOrRemoveSimpananMutation} = useMutation({
-        mutationFn: () => addOrRemoveSimpanan(id),
-        onMutate: () => {
-            setLoading(true)
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ['simpan']})
-            queryClient.invalidateQueries({
-                queryKey: ['detail-book', id]
-            })
-            setLoading(false)
-        },
-        onError: () => {
-            setLoading(false)
-        }
-    })
-
-    const handleClick = async() => {
-        await addOrRemoveSimpananMutation()
-    }
-
-    if (dataLoading || loading) {
+    if (queryLoading || mutationLoading) {
         return (
             <Button disabled className='w-8 h-8 border p-2 bg-transparent hover:bg-muted'>
                 <Loader2 className="w-8 h-8 dark:stroke-white stroke-primary animate-spin" />
@@ -43,13 +24,10 @@ const AddSimpananButton = ({id} : {id: string}) => {
         )
     }
 
-    const idBukuTersimpan = data?.bukuDisimpan.map((item: any) => item.buku._id.toString())
-    const isIncludes = idBukuTersimpan.includes(id)
-
     return (
         <>
-            <Button type='submit' onClick={handleClick} className={`w-8 h-8 border p-2 ${isIncludes ? 'dark:bg-primary-foreground bg-primary hover:bg-primary' : 'bg-transparent hover:bg-muted'}  duration-200 ease-in-out`}>
-                {dataLoading ? (
+            <Button type='submit' onClick={mutationFn} className={`w-8 h-8 border p-2 ${isIncludes ? 'dark:bg-primary-foreground bg-primary hover:bg-primary' : 'bg-transparent hover:bg-muted'}  duration-200 ease-in-out`}>
+                {queryLoading ? (
                     <Loader2 className="w-8 h-8 dark:stroke-white stroke-primary animate-spin" />
                 ) : (
                     <BookMarked className={`w-8 h-8 dark:stroke-white ${isIncludes && 'stroke-white'} stroke-black`} />
---
+++ client/src/hooks/fetchHooks/penggunaHooks/simpanHooks/useSimpanBukuPengguna.ts
@@ -0,0 +1,54 @@
+import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query"
+import { getAllSimpanan, addOrRemoveSimpanan } from "@/actions/simpanActions"
+import { errorMsgGenerator } from "@/utils/errorMsgFunc"
+import { toast } from "sonner"
+import { useNavigate } from "react-router-dom"
+
+type HooksProps = {
+    idBuku: string
+}
+
+const useSimpanBukuPengguna = ({idBuku} : HooksProps) => {
+    const navigate = useNavigate()
+    const queryClient = useQueryClient()
+
+    const {data, isLoading} = useQuery({
+        queryKey: ['simpan'],
+        queryFn: getAllSimpanan
+    })
+
+    const mutation = useMutation({
+        mutationFn: () => addOrRemoveSimpanan(idBuku),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['simpan']})
+            toast('Ditambahkan ke simpan', {
+                description: 'Lihat daftar buku yang disimpan',
+                action: {
+                    label: 'Lihat',
+                    onClick: () => navigate('/my/tersimpan')
+                }
+             })
+        },
+        onError: (error: any) => {
+            const errMsg = errorMsgGenerator(error) || 'Tidak dapat menambahkan ke simpan'
+            toast('Terjadi kesalahan', {description: errMsg})
+        }
+    })
+
+    const handleClick = () => {
+        mutation.mutate()
+    }
+
+    const idBukuTersimpan = data?.bukuDisimpan?.map((item: any) => item.buku._id.toString())
+    const isIncludes = idBukuTersimpan?.includes(idBuku)
+
+    return {
+        mutationLoading: mutation.isPending,
+        queryLoading: isLoading,
+        mutationFn: handleClick,
+        isIncludes,
+    }
+
+}
+
+export default useSimpanBukuPengguna
